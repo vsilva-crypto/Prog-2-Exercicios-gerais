2222e5c42f0a20093b54b4698e09b56a252a2494
vsilva-crypto <joao.vsilva280305@gmail.com>
2026-09-14 07:17:39 -0300

    Função para rodar o programa para uso de funções da biblioteca string_utils.

--- exercicios-gerais-main/03_bibliotecas/bibli_05/Respostas/JoaoVitor/main.c
+++ exercicios-gerais-main/03_bibliotecas/bibli_05/Respostas/JoaoVitor/main.c
@@ -1,19 +1,64 @@
 #include <stdio.h>
 #include "string_utils.h"
 
-void imprimeMenu()
+#define TAM_MAX_STRING 50
+
+void imprimeMenu ()
 {
-    printf ("1 - Tamanhon da string\n");
+    printf ("1 - Tamanho da string\n");
     printf ("2 - Copiar string\n");
     printf ("3 - Converter string para letras maiusculas\n");
     printf ("4 - Converter string para letras minusculas\n");
     printf ("5 - Imprimir string ao contrario\n");
     printf ("6 - Encerrar o programa\n");
-    printf ("Opcao escolhida: ");
+    printf ("Opcao escolhida: \n");
+}
+
+void executaMenu (char string[])
+{
+    int opcao = 0;
+    char destString[TAM_MAX_STRING];
+    
+    while (opcao != 6)
+    {
+        imprimeMenu();
+        scanf ("%d", &opcao);
+
+        switch (opcao)
+        {
+            case 1:
+            printf ("Tamanho da string: %d\n\n", string_length (string));
+            break;
+        
+            case 2:
+            string_copy (string, destString);
+            printf ("String copiada: %s\n\n", destString);
+            break;
+
+            case 3:
+            string_upper (string);
+            printf ("String convertida para maiusculas: %s\n\n", string);
+            break;
+
+            case 4: 
+            string_lower (string);
+            printf ("String convertida para minusculas: %s\n\n", string);
+            break;
+
+            case 5:
+            string_reverse (string);
+            printf ("String invertida: %s\n\n", string);
+            break;
+        }
+    }
 }
 
 int main ()
 {
-    imprimeMenu();
+    char string[TAM_MAX_STRING];
+    scanf ("%[^\n]", string);
+
+    executaMenu(string);
+
     return 0;
 }
